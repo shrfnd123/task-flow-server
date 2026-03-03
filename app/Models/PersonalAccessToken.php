492f87fbecb64ef7a52b4b2375af6d8ae5995ee8
nadja sharief <nadjasharief@nadjas-MacBook-Air.local>
2026-03-03 15:30:53 +0800

    personal token

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken extends Model
+class PersonalAccessToken extends SanctumPersonalAccessToken
 {
     //
 }
